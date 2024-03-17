6616554091bc0c9ee3fbfb731b74c59fb1da93d3
Kadifa <Kudickadifa@gmail.com>
2024-03-17 15:24:11 -0500

    practice

--- src/main/java/com/code/service/impl/EmployeeServiceImp.java
+++ src/main/java/com/code/service/impl/EmployeeServiceImp.java
@@ -1,8 +1,29 @@
 package com.code.service.impl;
 
+import com.code.entity.Employee;
+import com.code.repository.EmployeeRepository;
 import com.code.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EmployeeServiceImp implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImp(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee addEmployee(Employee employee) {
+        Employee savedEmployee = employeeRepository.save(employee);
+        return savedEmployee;
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
 }
