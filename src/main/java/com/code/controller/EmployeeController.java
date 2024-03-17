b507c89a637b60f1db34b08c75c4873a84be6a93
Kadifa <Kudickadifa@gmail.com>
2024-03-17 15:23:57 -0500

    practice

--- src/main/java/com/code/controller/EmployeeController.java
+++ src/main/java/com/code/controller/EmployeeController.java
@@ -1,26 +1,39 @@
 package com.code.controller;
 
 import com.code.entity.Employee;
-import com.code.repository.EmployeeRepository;
+
 import com.code.service.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/code")
 public class EmployeeController {
-    @Autowired
-    private EmployeeRepository employeeRepository;
 
-    @GetMapping
-    public List<Employee> getAllEmployee(){
-        System.out.println("getting all employees");
-       return  employeeRepository.findAll();
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+    private final EmployeeService employeeService;
+
+
+    @PostMapping("/save") //@RequestBody should be bound to the body of the web request
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+        Employee employeeSaved = employeeService.addEmployee(employee);
+        return new ResponseEntity<Employee>(employeeSaved, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> getAllEmployees(){
+        List<Employee> listOfAllEmployees = employeeService.getAllEmployees();
+        return new ResponseEntity<List<Employee>>(listOfAllEmployees, HttpStatus.OK);
 
     }
+
 
 }
