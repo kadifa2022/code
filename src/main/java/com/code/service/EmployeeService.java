b4fe6c7ec35f851a188ef649980ca8867ea1f53c
Kadifa <Kudickadifa@gmail.com>
2024-03-17 15:24:03 -0500

    practice

--- src/main/java/com/code/service/EmployeeService.java
+++ src/main/java/com/code/service/EmployeeService.java
@@ -1,4 +1,12 @@
 package com.code.service;
 
+import com.code.entity.Employee;
+
+import java.util.List;
+
 public interface EmployeeService {
+
+    Employee addEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
 }
