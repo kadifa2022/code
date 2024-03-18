de6a76db48cdefedf31c3e31ea9932e71ca56e1b
Kadifa <Kudickadifa@gmail.com>
2024-03-18 09:31:06 -0500

    practice

--- src/main/java/com/code/service/impl/EmployeeServiceImp.java
+++ src/main/java/com/code/service/impl/EmployeeServiceImp.java
@@ -1,11 +1,13 @@
 package com.code.service.impl;
 
+import com.code.custom_exception.BusinessException;
 import com.code.entity.Employee;
 import com.code.repository.EmployeeRepository;
 import com.code.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 
 @Service
 public class EmployeeServiceImp implements EmployeeService {
@@ -18,12 +20,31 @@ public class EmployeeServiceImp implements EmployeeService {
 
     @Override
     public Employee addEmployee(Employee employee) {
-        Employee savedEmployee = employeeRepository.save(employee);
-        return savedEmployee;
+        try {
+            if (employee.getName().isEmpty() || employee.getName().length() == 0) {
+                throw new BusinessException("601 ", "Please send proper name, It blank ");
+            }
+            Employee savedEmployee = employeeRepository.save(employee);
+            return savedEmployee;
+        } catch (IllegalArgumentException e) {
+            throw new BusinessException("602", "given employee is null" + e.getMessage());
+        } catch (Exception e) {
+            throw new BusinessException("603", "Something wnt wrong in Service layer" + e.getMessage());
+        }
     }
-
     @Override
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeById(Long employeeId) {
+        return employeeRepository.findById(employeeId).get();
+    }
+
+    @Override
+    public void deleteByEmpId(Long empId) {
+        employeeRepository.deleteById(empId);
+
     }
 }
