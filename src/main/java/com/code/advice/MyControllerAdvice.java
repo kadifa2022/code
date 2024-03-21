ec46eee243d3e69e7fbb5e1a6cc7f661257246fd
Kadifa <Kudickadifa@gmail.com>
2024-03-21 07:46:56 -0500

    practice

--- src/main/java/com/code/advice/MyControllerAdvice.java
+++ src/main/java/com/code/advice/MyControllerAdvice.java
@@ -1,18 +1,46 @@
 package com.code.advice;
 
 import com.code.custom_exception.EmptyInputException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+import java.util.NoSuchElementException;
 
 
 @ControllerAdvice // Global Exception Handling class
-public class MyControllerAdvice {
+public class MyControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EmptyInputException.class)
     public ResponseEntity<String> handleEmptyInput(EmptyInputException emptyInputException){
         return  new ResponseEntity<String>("Input field is Empty, Please look into it", HttpStatus.BAD_REQUEST);
     }
+    @ExceptionHandler(NoSuchElementException.class)
+    public  ResponseEntity<String> handleNoSuchElementException(NoSuchElementException noSuchElementException){
+        return new ResponseEntity<String>("No value is present in DB, Please change your request ", HttpStatus.NOT_FOUND);
+    }
+
+    @Nullable
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return  new ResponseEntity<Object>("Please change http method type", HttpStatus.NOT_FOUND);
+    }
+
+//
+//    @Override
+//    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException httpException,
+//                              HttpHeaders headers, HttpStatus status, WebRequest request){
+//        return  new ResponseEntity<Object>("Please change http method type", HttpStatus.NOT_FOUND);
+//
+//    }
+
+
 
 }
